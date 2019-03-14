9548ab2ee9d57411b4b86e34e106c368db97e5ba
magmajo <countrydev76vending@gmail.com>
2019-03-14 16:18:10 +0900

    add favicon and admin plugin

---
+++ hjjo_start.sh
@@ -0,0 +1,26 @@
+####################################
+## Homepage
+####################################
+## . jekyll
+##   https://jekyllrb.com/docs/
+##   https://jekyllrb-ko.github.io/docs/configuration/
+## 
+## . theme
+##   http://themes.jekyllrc.org/
+##   http://themes.jekyllrc.org/TeXt/
+##   https://tianqi.name/jekyll-TeXt-theme/docs/ 
+##
+## . favicon
+##   https://realfavicongenerator.net/
+
+####################################
+## Script
+####################################
+#wget https://github.com/kitian616/jekyll-TeXt-theme/archive/master.zip
+#mv master.zip jekyll-TeXt-theme-master.zip
+#unzip jekyll-TeXt-theme-master.zip
+#mv jekyll-TeXt-theme-master nblog-countrydev
+#cd nblog-countrydev
+
+bundle exec jekyll serve --port 14081
+
