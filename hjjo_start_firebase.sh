30bb7f667ca0d1d68aea4141c01f475ae5914235
magmajo <countrydev76@gmail.com>
2019-03-29 01:33:11 +0900

    modify shell script

---
+++ hjjo_start_firebase.sh
@@ -0,0 +1,8 @@
+## - [Firebase에 웹사이트 호스팅하기](https://blog.usefulparadigm.com/firebase%EC%97%90-%EC%9B%B9%EC%82%AC%EC%9D%B4%ED%8A%B8-website-%ED%98%B8%EC%8A%A4%ED%8C%85%ED%95%98%EA%B8%B0-3ed5d4fb5853)
+## - [Firebase 호스팅](https://firebase.google.com/docs/hosting/)
+## - [kiosk-android Firebase 호스팅](https://github.com/coderbunker/kiosk-android)
+
+#npm install -g firebase-tools
+#firebase login
+#firebase init
+firebase deploy
